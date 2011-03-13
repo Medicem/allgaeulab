53f3ebc775a3eb0e9d36b13fa6b72827d396783b
allgaeulab <edv@allgaeulab.de>
2011-03-14 00:57:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allgaeulab::Application.config.secret_token = 'e3335ea0d1c289078ef6bd770bbedd4f21d9014d6acbd7b1730344ac754336ac5599dcbe15db4c5a3ce8705d122e12b2547b613e4559ce798f5145811bc80b5c'
